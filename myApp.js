0294bb1355ee852cbce4d3ea5bc89f67430d9280
Ezex <ezequielmiranda87@gmail.com>
2018-07-25 14:25:57 -0300

    Finish express challenges

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 
 var express = require('express');
 var app = express();
+app.use(express.json())
+app.use(express.urlencoded())
 
 // --> 7)  Mount the Logger middleware here
 
@@ -9,31 +11,71 @@ var app = express();
 
 
 /** 1) Meet the node console. */
+console.log('Hello World')
 
-
-/** 2) A first working Express Server */
-
-
-/** 3) Serve an HTML file */
-
-
-/** 4) Serve static assets  */
-
-
-/** 5) serve JSON on a specific route */
-
-
-/** 6) Use the .env file to configure the app */
- 
  
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
+app.use((req, res, next)=>{
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
+
+
+/** 2) A first working Express Server */
+/*
+app.get('/', (req, res)=>{
+  res.send('Hello Express')
+})
+*/
+
+/** 3) Serve an HTML file */
+app.get('/', (req, res)=>{
+  // console.log(__dirname)  Return the absolute path
+  const absolutePath = __dirname + '/views/index.html'
+  res.sendFile(absolutePath)
+})
+
+
+/** 4) Serve static assets  */
+app.use(express.static(__dirname + '/public'))
+
+
+
+/** 5) serve JSON on a specific route */
+/** 6) Use the .env file to configure the app */
+app.get('/json', (req, res)=>{
+  const obj = {"message": "Hello json"}
+  if(process.env.MESSAGE_STYLE == 'uppercase')
+  {
+    console.log('si ')
+    obj.message = obj.message.toUpperCase()
+  }
+  
+  res.json(obj)
+  
+})
+
+
+ 
 
 /** 8) Chaining middleware. A Time server */
+app.get('/now',
+        (req, res, next)=>{ 
+          req.time = new Date().toString()
+          next()
+},
+        (req, res)=>{
+          res.json({"time":req.time})
+})
 
 
 /** 9)  Get input from client - Route parameters */
+app.get('/:word/echo', (req, res)=>{
+    res.json({"echo": req.params.word})
+})
+
 
 
 /** 10) Get input from client - Query parameters */
@@ -45,6 +87,16 @@ var app = express();
 
 
 /** 12) Get data form POST  */
+
+app.route('/name')
+  .get((req, res)=>{
+    res.json({name: `${req.query.first} ${req.query.last}`})
+  
+  })
+
+  .post((req, res)=>{
+    res.json({name: `${req.body.first} ${req.body.last}`})
+})
 
 
 
